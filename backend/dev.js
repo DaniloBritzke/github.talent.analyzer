9261fcb239342d5316b3c3d28d2d6cda62edf6a5
Danilo Britzke <danilo.britzke@ibstecnologia.com.br>
2024-09-20 09:37:15 -0300

    Feat: eslint config

--- backend/dev.js
+++ backend/dev.js
@@ -1,25 +1,25 @@
-const concurrently = require('concurrently');
+const concurrently = require('concurrently')
 
 //   "serve:back": "concurrently --raw  --kill-others --handle-input=false  \"\" \"\"",
 
 concurrently(
-  [
+    [
+        {
+            command: 'tsx watch src',
+            name: 'server',
+            env: { DEBUG_COLORS: 1 },
+        },
+        { command: 'nodemon --config nodemon.tsoa.json', name: 'tsoa' },
+    ],
     {
-      command: "tsx watch src",
-      name: 'server',
-      env: { DEBUG_COLORS: 1 },
+        prefix: 'name',
+        raw: true,
+        killOthers: ['failure'],
+        restartTries: 3,
+        pauseInputStreamOnFinish: false,
+        handleInput: false,
+        defaultInputTarget: 0,
+        inputStream: null,
+        timestampFormat: '-',
     },
-    { command: 'nodemon --config nodemon.tsoa.json', name: 'tsoa' },
-  ],
-  {
-    prefix: 'name',
-    raw: true,
-    killOthers: ['failure'],
-    restartTries: 3,
-    pauseInputStreamOnFinish: false,
-    handleInput: false,
-    defaultInputTarget: 0,
-    inputStream: null,
-    timestampFormat: '-',
-  },
-);
+)
